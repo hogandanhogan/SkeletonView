6de736b21b6cc8dc308af0bd57421c436866bc5c
Dan Hogan <hogandanhogan@gmail.com>
2020-10-06 10:27:14 -0400

    Initial Commit

---
+++ SkeletonView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkeletonView
+//
+//  Created by Dan Hogan on 10/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
